193c31e20aa253d0742517ae4837cb1c8099e412
IT21348570 <it21348570@my.sliit.lk>
2023-08-16 11:55:31 +0530

    Done it

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -4,6 +4,7 @@ import { MailchimpForm } from "./MailchimpForm";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
 import Img01 from '../assets/img/logo04.png';
 
 export const Footer = () => {
@@ -20,6 +21,7 @@ export const Footer = () => {
               <a href="https://www.linkedin.com/in/isharapramod-ranaweera/"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://web.facebook.com/?stype=lo&jlou=AfdvTzGfz0SOWTKv-T_TnR4ptJmyufs9LmJLeQXoNO77FUcgdD-B0ONnv3Qapi0bx4HLPdjKqLOtYBTWEamfHzlu9oHas9k0isFWLrBXZnTMdQ&smuh=32498&lh=Ac-b4tJpsJrJUIdCAyk"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon"/></a>
+              <a href="https://github.com/login"><img src={navIcon4} alt="Icon"/></a>
             </div>
           </Col>
         </Row>
